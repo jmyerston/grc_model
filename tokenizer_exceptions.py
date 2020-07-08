be6ad10ee02845251115bef5efee5081ecc85844
jmyerston <43222279+jmyerston@users.noreply.github.com>
2020-07-08 09:37:55 -0700

    Initial files added
    These are the main template files for the  grc model

---
+++ tokenizer_exceptions.py
@@ -0,0 +1,7 @@
+# coding: utf8
+from __future__ import unicode_literals
+
+from ...symbols import ORTH, LEMMA, TAG, NORM, PRON_LEMMA
+
+
+### thiw needs to be done by looking at other language models
